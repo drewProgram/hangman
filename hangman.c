ee77532f6a6ef70ae318e371b639c2120bfb7c8c
Andrew <andrew.filgueiras@outlook.com>
2020-08-21 21:34:18 -0300

    chore: refactorated code with functions

--- hangman.c
+++ hangman.c
@@ -2,50 +2,39 @@
 #include <string.h>
 #include <ctype.h>
 
+char secretWord[20];
+char guesses[26];
+int attempts = 0;
+
+void defineWord() {
+  sprintf(secretWord, "HANGMAN");
+}
+
 void opening() {
   printf("***************************\n");
   printf("*      HANGMAN GAME       *\n");
   printf("***************************\n\n");
 }
 
-void makeGuess(char guesses[26], int* attempts) {
-  char guess;
-    printf("Choose a letter: ");
-    scanf(" %c", &guess);
-    guess = toupper(guess);
+int hasGuessed(char letter) {
+  int found = 0;
 
-    guesses[*attempts] = guess;
-    (*attempts)++;
+  for (int j = 0; j < attempts; j++)
+  {
+    if (guesses[j] == letter)
+    {
+      found = 1;
+      break;
+    }
+  }
+
+  return found;
 }
 
-int main() {
-  char secretWord[20];
-
-  // prints a string inside an array
-  sprintf(secretWord, "HANGMAN");
-
-  int won = 0;
-  int hanged = 0;
-
-  char guesses[26];
-  int attempts = 0;
-
-  opening();
-
-  do
-  {
-    for (int i = 0; i < strlen(secretWord); i++)
+void drawHanger() {
+  for (int i = 0; i < strlen(secretWord); i++)
     {
-      int found = 0;
-
-      for (int j = 0; j < attempts; j++)
-      {
-        if (guesses[j] == secretWord[i])
-        {
-          found = 1;
-          break;
-        }
-      }
+      int found = hasGuessed(secretWord[i]);
 
       if (found)
       {
@@ -57,8 +46,30 @@ int main() {
       }
     }
     printf("\n\n");
+}
 
-    makeGuess(guesses, &attempts);
+void makeGuess() {
+  char guess;
+    printf("Choose a letter: ");
+    scanf(" %c", &guess);
+    guess = toupper(guess);
+
+    guesses[attempts] = guess;
+    attempts++;
+}
+
+int main() {
+  int won = 0;
+  int hanged = 0;
+
+  defineWord(secretWord);
+  opening();
+
+  do
+  {
+    drawHanger();
+
+    makeGuess();
 
   } while (!won && !hanged);
 
